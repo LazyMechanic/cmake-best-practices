42b1a356197e92f0e3065253603329fce817ebf1
LazyMechanic <AsharnRus@gmail.com>
2019-10-18 22:57:40 +0300

    Add executable example

--- src/library/library.cpp
+++ src/library/library.cpp
@@ -1,16 +1,8 @@
-#include "cmake_best_practice_library.h"
+#include "library.h"
 
 #include <iostream>
 
-#include <yaml-cpp/yaml.h>
-
 void cmake_best_practice_library::print()
 {
-    auto doc = YAML::Load(
-        "---"
-        "First:"
-        "  - First_second"
-        "...");
-    std::cout << "cmake_best_practice_library::print()" << std::endl;
-    std::cout << YAML::Dump(doc) << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 }
